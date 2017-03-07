3489b24a6088a8b6f3e6a2e017c12c398f1735ed
Derek <a0985265734@gmail.com>
2017-03-07 14:05:47 +0800

    users page working without photos

---
+++ app/Http/Requests/UsersRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class UsersRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'=>'required',
+            'email'=>'required',
+            'role_id'=>'required',
+            'is_active'=>'required',
+            'password'=>'required'
+            //
+        ];
+    }
+    public function messages(){
+        return [
+            'name.required'=>'姓名不可為空白',
+            'email.required'=>'email不可為空白',
+            'role_id.required'=>'Role不可為空白',
+            'is_active.required'=>'Status不可為空白',
+            'password.required'=>'密碼不可為空白'
+        ];
+    }
+}
